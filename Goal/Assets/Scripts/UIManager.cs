3902ac900a2db673dda3d8839b0a4163303e28a9
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-12-16 11:44:00 +0100

    Added win & lose feedback & script
    Added a UIManager, in it there are two public functions
    
    WinAnimation();
    LoseAnimation();

---
+++ Goal/Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private RawImage goalImage;
+    [SerializeField] private RawImage confettiImage;
+    [SerializeField] private RawImage loseImage; 
+
+    private Color transparent;
+
+    private Color initialColor;
+
+    private void Awake(){
+        initialColor = goalImage.color;
+        loseImage.color = transparent;
+        goalImage.color = transparent;
+        confettiImage.color = transparent;
+    }
+
+    public void WinAnimation(){
+        goalImage.color = initialColor;
+        confettiImage.color = initialColor;
+    }
+    public void LoseAnimation(){
+        loseImage.color = initialColor;
+    }
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.E)){
+            WinAnimation();
+        }
+        if(Input.GetKeyDown(KeyCode.F)){
+            LoseAnimation();
+        }
+    }
+}
